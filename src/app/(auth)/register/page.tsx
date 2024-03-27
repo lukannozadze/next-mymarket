46bdbb4024353efeca329f56c77cb87c7da76007
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-03-27 16:10:02 +0400

    add authorization page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,6 +6,7 @@ import { signUpWithEmailAndPassword } from "../actions";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
+import Link from "next/link";
 const formSchema = z
   .object({
     email: z.string().email({message:'Enter the Email'}).min(1),
@@ -66,15 +67,15 @@ export default function RegisterForm() {
   }
   return (
     <div className="flex flex-col items-center justify-center mt-16 ">
-      <Image src="/auth-logo.svg" alt="auth-tnet" width={185} height={33} />
+      <Image src="/tnet-en.svg" alt="auth-tnet" width={185} height={33} />
       <form
         className="flex flex-col pt-[92px] w-[50%] items-center"
         onSubmit={form.handleSubmit(onSubmit)}
       >
         <div className="w-[90%] flex flex-col items-center gap-4">
-          <h2 className="w-[70%] text-3xl font-bold">Create Account</h2>
+          <h2 className="w-[60%] text-3xl font-bold">Create Account</h2>
           <div className="w-[100%] flex flex-col items-center gap-3">
-            <div className="w-[70%] flex flex-col  "> 
+            <div className="w-[60%] flex flex-col  "> 
               <Input
               className="w-[100%] p-4 rounded-lg"
               {...form.register("email")}
@@ -85,7 +86,7 @@ export default function RegisterForm() {
                 <Image src='/icons/error.png' alt="error" width={25} height={25}/>
                 <span className=" text-[#FF0000]">{form.formState.errors.email.message}</span></div>}
             </div>
-            <div className="w-[70%] flex flex-col  "> 
+            <div className="w-[60%] flex flex-col  "> 
               <Input
               className="w-[100%] p-4 rounded-lg"
               {...form.register("password")}
@@ -97,7 +98,7 @@ export default function RegisterForm() {
                 <span className=" text-[#FF0000]">{form.formState.errors.password.message}</span></div>}
             </div>
             
-            <div className="w-[70%] flex flex-col  "> 
+            <div className="w-[60%] flex flex-col  "> 
               <Input
               className="w-[100%] p-4 rounded-lg"
               {...form.register("confirm")}
@@ -108,7 +109,7 @@ export default function RegisterForm() {
                 <Image src='/icons/error.png' alt="error" width={25} height={25}/>
                 <span className=" text-[#FF0000]">{form.formState.errors.confirm.message}</span></div>}
             </div>
-            <div className="w-[70%] flex flex-col  "> 
+            <div className="w-[60%] flex flex-col  "> 
               <Input
               className="w-[100%] p-4 rounded-lg"
               {...form.register("name")}
@@ -119,7 +120,7 @@ export default function RegisterForm() {
                 <Image src='/icons/error.png' alt="error" width={25} height={25}/>
                 <span className=" text-[#FF0000]">{form.formState.errors.name.message}</span></div>}
             </div>
-            <div className="w-[70%] flex flex-col  "> 
+            <div className="w-[60%] flex flex-col  "> 
               <Input
               className="w-[100%] p-4 rounded-lg"
               {...form.register("lastname")}
@@ -130,7 +131,7 @@ export default function RegisterForm() {
                 <Image src='/icons/error.png' alt="error" width={25} height={25}/>
                 <span className=" text-[#FF0000]">{form.formState.errors.lastname.message}</span></div>}
             </div>
-            <div className="w-[70%] flex flex-col  "> 
+            <div className="w-[60%] flex flex-col  "> 
               <Input
               className="w-[100%] p-4 rounded-lg"
               {...form.register("telephone")}
@@ -149,13 +150,18 @@ export default function RegisterForm() {
           </div>
           <button
             onClick={() => console.log(form.formState.errors)}
-            className="font-bold mt-4 w-[70%] text-white bg-[#3C74FF] rounded-full py-3"
+            className="font-bold mt-4 w-[60%] text-white bg-[#3C74FF] rounded-full py-3"
             type="submit"
           >
             Confirm
           </button>
         </div>
       </form>
+
+      <div className="flex gap-2 mt-5">
+      <span className="text-[#979797]">With an existing account</span>
+      <Link className="text-[#3C74FF] hover:text-black" href='/login'>Log In</Link>
+      </div>
     </div>
   );
 }
