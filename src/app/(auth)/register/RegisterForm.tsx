74d5ec66378e9a8773b5f23fe38a4e8a675f6ee8
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-04-01 13:34:26 +0400

    fix vercel issue - comment

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -218,11 +218,6 @@ export default function RegisterForm() {
                 </div>
               )}
             </div>
-
-            {/* <div className="w-[70%] flex items-center gap-3">
-              <Checkbox {...form.register("rule")} className="w-5 h-5" />
-              <span>ვეთანხმები წესებსა და პირობებს</span>
-            </div> */}
           </div>
           <button
             onClick={() => console.log(form.formState.errors)}
