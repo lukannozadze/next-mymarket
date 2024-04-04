0f48a816657f0ba5c7bdb7ee9acc97f81e0e35cd
Luka Nozadze <nozadzeluka2002@gmail.com>
2024-04-04 17:04:08 +0400

    fix server error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
           <form action={signOut}>
             <button>Log Out</button>
             <Categories/>
-            {/* <Products/> */}
+             <Products/>
           </form>
         </main>
         <Footer />
